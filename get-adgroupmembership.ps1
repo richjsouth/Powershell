7165eaa7de48e1125b90b264c16f303e0414c644
richjsouth <30663346+richjsouth@users.noreply.github.com>
2017-09-14 08:32:23 +0100

    Update get-adgroupmembership.ps1
    Allow the group name to be input on the command-line.

--- get-adgroupmembership.ps1
+++ get-adgroupmembership.ps1
@@ -1,9 +1,8 @@
 # Powershell script to display which usernames and user are in a specific AD group 
-# RS - 18/08/2017
+# RS - 29/08/2017 - Accept group as command-line argument
 
-# Prompt for group name and store in $groupname variable
-$groupname = read-host "Enter group name"
+param([string]$groupName = "groupname")
 
 # Look for the members in $group, pass each username in turn into get-aduser 
 # Dispaly only the user's username and their real name (stored in Display Name) 
-get-ADGroupMember -identity "$groupname" -Recursive | Get-ADUser -Property DisplayName | Select Name,DisplayName
+get-ADGroupMember -identity "$groupName" -Recursive | Get-ADUser -Property DisplayName | Select Name,DisplayName
