bb5d1a4e926b420201d73986d1c415b6b79a2fcd
richjsouth <30663346+richjsouth@users.noreply.github.com>
2019-04-04 12:05:30 +0100

    Delete get-adgroupmembership.ps1

--- get-adgroupmembership.ps1
+++
@@ -1,8 +0,0 @@
-# Powershell script to display which usernames and user are in a specific AD group 
-# RS - 29/08/2017 - Accept group as command-line argument
-
-param([string]$groupName = "groupname")
-
-# Look for the members in $group, pass each username in turn into get-aduser 
-# Dispaly only the user's username and their real name (stored in Display Name) 
-get-ADGroupMember -identity "$groupName" -Recursive | Get-ADUser -Property DisplayName | Select Name,DisplayName
